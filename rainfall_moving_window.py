6eade121a005d2684d018b00bde0aa79d8aa9dba
Andy Wickert <andy@northernwidget.com>
2017-07-20 22:58:50 -0500

    Argparse!

--- rainfall_moving_window.py
+++ rainfall_moving_window.py
@@ -14,9 +14,38 @@ import numpy as np
 import sys
 from matplotlib import pyplot as plt
 import os
+import argparse
 
-filename = sys.argv[1]
-window = float(sys.argv[2]) # minutes
+parser = argparse.ArgumentParser(description= \
+        'Compute rainfall rate with time from tipping-bucket rain gauge data.',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+window = 60. # default
+parser.add_argument('--filename', type=str, nargs=1, default=argparse.SUPPRESS,
+                    help='ASCII file containing bucket-tip time stamps')
+parser.add_argument('--logger', type=str, nargs=1, default=argparse.SUPPRESS,
+                    help='Which type of data logger?',
+                    choices=['alog', 'hobo'])
+parser.add_argument('--window', type=float, nargs=1, default=60,
+                    help='smoothing window duration [minutes]')
+
+args = parser.parse_args()
+args = vars(args)
+if type(args['window']) != float:
+  window = args['window'][0]
+
+parser.add_argument('--ts', type=float, nargs=1,default=window/2,
+                    help='Time step for moving window; \
+                    defaults to (window/2)')
+                    
+args = parser.parse_args()
+args = vars(args)
+
+filename = args['filename'][0]
+logger = args['logger'][0]
+if type(args['ts']) != float:
+  dt = args['ts'][0]
+
 
 halfwin=window/2.
 halfwin*=60
@@ -24,6 +53,23 @@ dt=window/2. # minutes
 dt*=60 # convert to seconds
 
 rainread = csv.reader(open(filename,'rb'), delimiter=',')
+
+if logger == 'hobo':
+  firstline = rainread.next()[0]
+  secondline = rainread.next()
+  site_name = firstline.split("Plot Title: ")[-1]
+  # Rain column
+  rain_header = [s for s in secondline if "Event" in s]
+  rain_column_number = np.ravel(np.where(
+                       np.array(secondline) == rain_header))[0]
+  if ' in ' in rain_header[0]:
+    rain_units = 'inches'
+    conversion_to_mm = 25.4
+  else
+    sys.exit()
+
+  logger_serial_number
+  
 
 # Create a vector of time stamps
 tiptimes=[]
@@ -84,7 +130,7 @@ localname = os.path.split(filename)[-1]
 basename = os.path.splitext(localname)[0]
 outname = basename + '__'+str(window)+'_minute_moving_window'
 outdata = np.vstack(rainfall_rate_in_window).transpose()
-np.savetxt(outname+'.txt', outdata)
+#np.savetxt(outname+'.txt', outdata)
 
 ########
 # PLOT #
@@ -95,5 +141,6 @@ plt.plot(realtimes, rainfall_rate_in_window)
 plt.xticks( rotation = 45 )
 plt.ylabel('Rainfal rate [mm/hr]\n'+str(window)+'-minute moving window')
 plt.tight_layout()
-plt.savefig(outname+'.png')
+#plt.savefig(outname+'.png')
 plt.show()
+
